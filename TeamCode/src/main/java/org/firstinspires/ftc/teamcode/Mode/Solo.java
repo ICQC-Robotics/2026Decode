a02b64a66b8cf92c7b5c4b2ce2cc2b5040575fbd
Abdullah <52929221+abdulahmd@users.noreply.github.com>
2025-10-26 16:55:20 -0500

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/Solo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/Solo.java
@@ -56,6 +56,7 @@ public class Solo extends CommandOpMode {
                        GamepadKeys.Button.A,
                        new AutoAim(negabot.vision,
                                    negabot.shooter,
+                                   negabot.intake,
                                    negabot.drive,
                                    negabot.wait,
                                    negabot.isBlueAlliance()
