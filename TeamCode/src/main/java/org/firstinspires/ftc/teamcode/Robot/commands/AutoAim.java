df347870d49c6129970aba5c6b6bb1c08a1dea05
Abdullah <52929221+abdulahmd@users.noreply.github.com>
2025-10-28 01:53:54 -0500

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
@@ -60,7 +60,7 @@ public class AutoAim extends SequentialCommandGroup {
 
                     Actions.runBlocking(
                             mD.actionBuilder(mD.localizer.getPose())
-                                    .turnTo(heading)
+                                    .turnTo(mD.localizer.getPose().heading.toDouble() - heading)
                                     .build()
                     );
                 }),
