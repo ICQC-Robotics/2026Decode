907981d631a4f9ecc66ca1c4cb86f0dfd4a88ab4
Abdullah <52929221+abdulahmd@users.noreply.github.com>
2025-10-28 01:57:28 -0500

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
@@ -56,11 +56,11 @@ public class AutoAim extends SequentialCommandGroup {
                     if (!vision.hasTarget()) return;
                     double botX = vision.getBotX();
                     double botY = vision.getBotY();
-                    double heading = Math.atan2(botY, botX);
+                    double heading = Math.atan2(botY, -botX);
 
                     Actions.runBlocking(
                             mD.actionBuilder(mD.localizer.getPose())
-                                    .turnTo(mD.localizer.getPose().heading.toDouble() - heading)
+                                    .turnTo(heading)
                                     .build()
                     );
                 }),
