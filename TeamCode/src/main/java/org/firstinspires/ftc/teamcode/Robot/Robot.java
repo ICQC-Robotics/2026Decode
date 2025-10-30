bd69d0738bb0a885839286c386b5d50c2c5138c4
irfan.siddiqui@g.austincc.edu <141471703+IrfSid@users.noreply.github.com>
2025-10-30 15:09:50 -0500

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -44,6 +44,7 @@ public class Robot {
 
         shooter = new Shooter(
                 h.get(DcMotorEx.class, "6"), DcMotorSimple.Direction.FORWARD,
+                h.get(DcMotorEx.class, "7"), DcMotorSimple.Direction.FORWARD,
                 h.get(Servo.class, "shooter"),
                 new PIDFCoefficients(1000.0, 0.0, 0.1, 90.0)
         );
