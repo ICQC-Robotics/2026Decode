1f9a7dcc5f18c80f8f6a7a1e7ba0f99f8bf38262
Abdullah <52929221+abdulahmd@users.noreply.github.com>
2025-10-30 20:47:46 -0500

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
@@ -30,17 +30,15 @@ public class AutoAim extends SequentialCommandGroup {
 
     public AutoAim(Vision vision, Shooter shooter, Intake intake, Drive drive, Wait wait, boolean isBlueAlliance) {
         targetTagId = isBlueAlliance ? 20 : 24;
-        if(vision.hasTarget(targetTagId)) {
-            addCommands(
-                    new ParallelCommandGroup(
-                            new AutoIntake(intake, shooter).accept(),
-                            AimCommand(vision, shooter, drive, wait),
-                            new SequentialCommandGroup(
-                                    ShootCommand(vision, shooter, intake, wait)
-                            )
-                    )
-            );
-        }
+        addCommands(
+                new ParallelCommandGroup(
+                        new AutoIntake(intake, shooter).accept(),
+                        AimCommand(vision, shooter, drive, wait),
+                        new SequentialCommandGroup(
+                                ShootCommand(vision, shooter, intake, wait)
+                        )
+                )
+        );
     }
 
     private SequentialCommandGroup AimCommand(Vision vision, Shooter shooter, Drive drive, Wait wait) {
