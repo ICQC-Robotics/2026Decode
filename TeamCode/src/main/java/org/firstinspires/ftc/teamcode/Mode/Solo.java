0d501622dc6144dc68b5ecf0aa74e6d84e7834ac
Abdullah <52929221+abdulahmd@users.noreply.github.com>
2025-10-30 21:31:34 -0500

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/Solo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/Solo.java
@@ -3,10 +3,8 @@ package org.firstinspires.ftc.teamcode.Mode;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Robot.Robot;
 import org.firstinspires.ftc.teamcode.Robot.commands.AutoAim;
 import org.firstinspires.ftc.teamcode.Robot.commands.AutoIntake;
@@ -15,12 +13,12 @@ import org.firstinspires.ftc.teamcode.Robot.commands.SetBlueAlliance;
 import org.firstinspires.ftc.teamcode.Robot.commands.SetRedAlliance;
 
 @TeleOp(group=".")
-public class Solo extends OpMode {
+public class Solo extends CommandOpMode {
     GamepadEx g;
     Robot negabot;
 
     @Override
-    public void init() {
+    public void initialize() {
         g = new GamepadEx(gamepad1);
         negabot = new Robot(hardwareMap, g, null, true);
 
@@ -40,9 +38,9 @@ public class Solo extends OpMode {
 
 
         negabot.Action(g,
-                GamepadKeys.Button.RIGHT_BUMPER,
-                new AutoIntake(negabot.intake, negabot.shooter).accept(),
-                new AutoIntake(negabot.intake, negabot.shooter).finish()
+                       GamepadKeys.Button.RIGHT_BUMPER,
+                       new AutoIntake(negabot.intake, negabot.shooter).accept(),
+                       new AutoIntake(negabot.intake, negabot.shooter).finish()
         );
 
         negabot.Action(g,
@@ -52,23 +50,18 @@ public class Solo extends OpMode {
         );
 
         negabot.Action(g,
-                GamepadKeys.Button.A,
-                new AutoAim(negabot.vision,
-                        negabot.shooter,
-                        negabot.intake,
-                        negabot.drive,
-                        negabot.wait,
-                        negabot.isBlueAlliance()
-                ),
-                null
+                       GamepadKeys.Button.A,
+                       new AutoAim(negabot.vision,
+                                   negabot.shooter,
+                                   negabot.intake,
+                                   negabot.drive,
+                                   negabot.wait,
+                                   negabot.isBlueAlliance()
+                       ),
+                       null
         );
     }
 
-    @Override
-    public void loop() {
-        telemetry.addData("X", negabot.vision.getBotX());
-        telemetry.addData("Y", negabot.vision.getBotY());
-        telemetry.update();
-    }
+
 }
 
