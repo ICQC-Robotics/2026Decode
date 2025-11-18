15c10266396bbd894805787398ceee95c74973d7
SRQCS <shariqsk7677@gmail.com>
2025-11-17 20:33:42 -0600

    Update AutoAim.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/TeleOp/AutoAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/TeleOp/AutoAim.java
@@ -11,3 +11,21 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "AutoAim", group = "TeleOp")
 
+package org.firstinspires.ftc.teamcode.Mode.TeleOp;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+
+@TeleOp(name = "AutoAim", group = "TeleOp")
+public class AutoAim extends LinearOpMode {
+
+    
