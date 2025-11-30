a4a030a2eff821fc247afafb164ed63c1ddfc761
Abdullah <52929221+abdulahmd@users.noreply.github.com>
2025-11-30 11:21:49 -0600

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/TruePedro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/TruePedro.java
@@ -25,7 +25,85 @@ public class TruePedro extends CommandOpMode {
     Follower follower = negabot.drive.follower;
     PathChain moveToShoot, moveToGrab;
 
-    public void path() {
+    public void redClose() {
+        moveToShoot = follower.pathBuilder()
+                .addPath(new BezierCurve(
+                        new Pose(0, 0, 0),
+                        new Pose(10, 10, 10)
+                ))
+                .addPath(new BezierCurve(
+                        new Pose(10, 10, 10),
+                        new Pose(20, 20, 20)
+                ))
+                .setLinearHeadingInterpolation(0, 20)
+                .build();
+
+        moveToGrab = follower.pathBuilder()
+                .addPath(new BezierCurve(
+                        new Pose(20, 20, 20),
+                        new Pose(30, 30, 30)
+                ))
+                .addPath(new BezierCurve(
+                        new Pose(30, 30, 30),
+                        new Pose(40, 40, 40)
+                ))
+                .setLinearHeadingInterpolation(20, 40)
+                .build();
+    }
+
+    public void redFar() {
+        moveToShoot = follower.pathBuilder()
+                .addPath(new BezierCurve(
+                        new Pose(0, 0, 0),
+                        new Pose(10, 10, 10)
+                ))
+                .addPath(new BezierCurve(
+                        new Pose(10, 10, 10),
+                        new Pose(20, 20, 20)
+                ))
+                .setLinearHeadingInterpolation(0, 20)
+                .build();
+
+        moveToGrab = follower.pathBuilder()
+                .addPath(new BezierCurve(
+                        new Pose(20, 20, 20),
+                        new Pose(30, 30, 30)
+                ))
+                .addPath(new BezierCurve(
+                        new Pose(30, 30, 30),
+                        new Pose(40, 40, 40)
+                ))
+                .setLinearHeadingInterpolation(20, 40)
+                .build();
+    }
+
+    public void blueClose() {
+        moveToShoot = follower.pathBuilder()
+                .addPath(new BezierCurve(
+                        new Pose(0, 0, 0),
+                        new Pose(10, 10, 10)
+                ))
+                .addPath(new BezierCurve(
+                        new Pose(10, 10, 10),
+                        new Pose(20, 20, 20)
+                ))
+                .setLinearHeadingInterpolation(0, 20)
+                .build();
+
+        moveToGrab = follower.pathBuilder()
+                .addPath(new BezierCurve(
+                        new Pose(20, 20, 20),
+                        new Pose(30, 30, 30)
+                ))
+                .addPath(new BezierCurve(
+                        new Pose(30, 30, 30),
+                        new Pose(40, 40, 40)
+                ))
+                .setLinearHeadingInterpolation(20, 40)
+                .build();
+    }
+
+    public void blueFar() {
         moveToShoot = follower.pathBuilder()
                 .addPath(new BezierCurve(
                         new Pose(0, 0, 0),
@@ -54,7 +132,11 @@ public class TruePedro extends CommandOpMode {
     @Override
     public void initialize() {
         negabot = new Robot(hardwareMap, telemetry);
-        path();
+        telemetry.addLine("X for Red Close, Y for Red Far, A for Blue Close, B for Blue Far");
+        if(gamepad1.x) redClose();
+        if(gamepad1.y) redFar();
+        if(gamepad1.a) blueClose();
+        if(gamepad1.b) blueFar();
 
         negabot.schedule(
                 new SequentialCommandGroup(
