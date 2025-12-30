1f85cfa187be3c6d25d7ce6aded054eacd1bcbe6
nazim <nazimayub786@gmail.com>
2025-12-29 21:33:02 -0600

    s

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Tuner/ShooterTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Tuner/ShooterTuner.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Robot.Tuner;
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -20,9 +22,8 @@ public class ShooterTuner extends OpMode {
 
     Servo servo1, servo3;
     public static double s1 = 0.5;
-
     public static double targetRPM = 3000;
-
+    private Limelight3A limelight;
     private Robot negabot;
 
     @Override
@@ -32,17 +33,25 @@ public class ShooterTuner extends OpMode {
         );
         servo1 = hardwareMap.get(Servo.class, "servo1");
         servo3 = hardwareMap.get(Servo.class, "servo3");
+        limelight = hardwareMap.get(Limelight3A.class, "ll");
+        limelight.start();
     }
 
     @Override
     public void loop() {
+        LLResult result = limelight.getLatestResult();
+        double distance = (29.5 - 12) / Math.tan(Math.toRadians(12 + result.getTy()));
+
         negabot.shooter.setPIDF(kP, kI, kD, kF);
         negabot.shooter.setVelocity(targetRPM);
         negabot.intake.setSpeed(-1);
         servo1.setPosition(s1);
         servo3.setPosition(0.47);
+
+        telemetry.addData("distance", distance);
         telemetry.addData("Target RPM", targetRPM);
         telemetry.addData("Actual RPM", negabot.shooter.getVelocity());
         telemetry.update();
+
     }
 }
