b793395c309bcff78308223932c76cb50f475335
Naz <nazimayub786@gmail.com>
2025-12-30 18:46:44 -0600

    x

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PP/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PP/Constants.java
@@ -17,12 +17,12 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             //TODO: test and retune with the new mass
-            .mass(14.10672);
-//            .forwardZeroPowerAcceleration(-32.448)
-//            .lateralZeroPowerAcceleration(-61.192)
-//            .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0))
-//            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, .02, 0))
-//            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1,0.0,0.0001,0.6,0.0));
+            .mass(14.10672)
+            .forwardZeroPowerAcceleration(-32.1136666667)
+            .lateralZeroPowerAcceleration(-65.32766)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.1, 0, 0.01, 0.01))
+            .headingPIDFCoefficients(new PIDFCoefficients(1, 0, .02, 0.01))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1,0.0,0.0001,0.6,0.0));
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
@@ -36,8 +36,8 @@ public class Constants {
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(77.868)
-            .yVelocity(57.490);
+            .xVelocity(75.4666666667)
+            .yVelocity(54.9051);
 
 
 
