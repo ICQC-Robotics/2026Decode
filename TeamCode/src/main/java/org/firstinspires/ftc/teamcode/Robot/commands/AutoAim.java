486d2143eabc2707a7c8e1576ea6921052c96084
nazim <nazimayub786@gmail.com>
2025-12-31 13:45:21 -0600

    d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/commands/AutoAim.java
@@ -88,12 +88,14 @@ public class AutoAim extends SequentialCommandGroup {
 
                 new SequentialCommandGroup(
                         new InstantCommand(() -> shooter.setMagazineCover(Positions.OPEN_COVER.getPos()), shooter),
-                        new WaitCommand(wait, 1),
+                        new WaitCommand(wait, 1)),
 
                         new InstantCommand(() -> {
                             if (Double.isNaN(lastValidDistanceIn)) return;
 
-                            double hoodPosF = shooter.hood.getPosition() + calculateCoverIncrease(lastValidDistanceIn);
+                            double hoodPosF = shooter.hood.getPosition()
+                                    + calculateCoverIncrease(lastValidDistanceIn);
+
                             if (hoodPosF > 1.0) hoodPosF = 1.0;
                             if (hoodPosF < 0.0) hoodPosF = 0.0;
 
@@ -129,7 +131,7 @@ public class AutoAim extends SequentialCommandGroup {
                             intake.setSpeed(0);
                             lastValidDistanceIn = Double.NaN;
                         }, shooter, intake)
-                ));
+        );
     }
 
     private double updateRPM(Vision vision, Shooter shooter) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Turret.java
@@ -105,13 +105,7 @@ public class Turret extends SubsystemBase {
 
             double current = getAngleDeg();
             double desired = current + tx;
-
-            if (desired > MAX_DEG) {
-                desired = current - Math.abs(tx);
-            }
-            else if (desired < MIN_DEG) {
-                desired = current + Math.abs(tx);
-            }
+            
             desired = clamp(desired, MIN_DEG, MAX_DEG);
             setTargetDeg(desired);
             visionTxDeg = Double.NaN;
