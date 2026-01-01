1ba9512355cd538ad2bfa05dbc8164d2066cee97
Abdullah <52929221+abdulahmd@users.noreply.github.com>
2025-12-31 18:31:55 -0600

    m

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/PedroAutos/backAndForth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/PedroAutos/backAndForth.java
@@ -64,18 +64,16 @@ public class backAndForth extends OpMode {
     @Override
     public void init() {
         negabot = new Robot(hardwareMap, telemetry);
-        Follower follower = negabot.drive.follower;
-
         negabot.reset();
 
-        buildPaths(follower);
-        follower.setStartingPose(new Pose(72, 72, 90));
+        buildPaths(negabot.drive.follower);
+        negabot.drive.setStart(new Pose(72, 72, 90));
 
         SequentialCommandGroup autoSequence = new SequentialCommandGroup(
-                new FollowPathCommand(follower, Path1, true),
-                new FollowPathCommand(follower, Path2, true),
-                new FollowPathCommand(follower, Path3, true),
-                new FollowPathCommand(follower, Path4, true)
+                new FollowPathCommand(negabot.drive.follower, Path1, true),
+                new FollowPathCommand(negabot.drive.follower, Path2, true),
+                new FollowPathCommand(negabot.drive.follower, Path3, true),
+                new FollowPathCommand(negabot.drive.follower, Path4, true)
         );
 
         negabot.schedule(autoSequence);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Drive.java
@@ -92,6 +92,10 @@ public class Drive extends SubsystemBase {
         return follower.getPose().getHeading();
     }
 
+    public void setStart(Pose p) {
+        follower.setStartingPose(p);
+        follower.setPose(p);
+    }
     public void turnInPlace(double turnPower) {
         fL.setPower(turnPower);
         bL.setPower(turnPower);
