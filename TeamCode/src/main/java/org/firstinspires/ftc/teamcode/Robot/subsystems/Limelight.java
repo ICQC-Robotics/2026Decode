6d648581b06afc3d076a006725e78d975d13b248
Zayan Hassan <n0tzayan2022@gmail.com>
2026-02-20 23:43:24 -0600

    etb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Limelight.java
@@ -4,12 +4,18 @@ import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import com.pedropathing.geometry.Pose;
+
 
 public class Limelight extends SubsystemBase {
 
     private final Limelight3A limelight;
     private LLResult lastResult;
+    public final double turretRadius = 0;
+    public final double turretOffsetX = 0;
+    public final double turretOffsetY = 0;
 
     public Limelight(Limelight3A vision) {
         limelight = vision;
@@ -22,9 +28,16 @@ public class Limelight extends SubsystemBase {
         lastResult = limelight.getLatestResult();
     }
 
-    public Pose3D getBotposeMT2() {
+    public Pose getBotposeMT1(int t) {
         if (lastResult == null || !lastResult.isValid()) return null;
-        return lastResult.getBotpose_MT2();
+        t -= 135;
+        t *= -1;
+        Pose3D pose = lastResult.getBotpose();
+        Pose p = new Pose(pose.getPosition().x, pose.getPosition().y, pose.getOrientation().getYaw(AngleUnit.RADIANS));
+        p.setHeading(p.getHeading() + t);
+        p = p.withX(p.getX() + turretRadius * Math.sin(t));
+        p = p.withY(p.getY() - turretRadius * Math.cos(t));
+        return p.withX(p.getX() - turretOffsetX).withY(p.getY() - turretOffsetY);
     }
 
     public void updateRobotYawDeg(double robotYawDeg) {
