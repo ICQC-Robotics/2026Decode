b1588bc4e09410703d9c1bf6fa61c6e6fa53f34c
Naz <nazimayub786@gmail.com>
2026-02-24 21:17:12 -0600

    chud

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/SoloRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mode/SoloRed.java
@@ -145,13 +145,6 @@ public class SoloRed extends CommandOpMode {
             shooterStandby = true;
         }
 
-
-        Pose pose = negabot.drive.follower.getPose();
-        telemetry.addData("X (in)", pose.getX());
-        telemetry.addData("Y (in)", pose.getY());
-        telemetry.addData("Heading (deg)", Math.toDegrees(pose.getHeading()));
-        telemetry.update();
-
         Robot.LAST_POSE = negabot.drive.follower.getPose().copy();
         negabot.run();
     }
