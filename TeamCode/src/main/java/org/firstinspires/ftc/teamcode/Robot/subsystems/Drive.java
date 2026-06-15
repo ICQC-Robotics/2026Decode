79a99b3ed1990e48ffaebb1a928f5a0282e35381
Naz <nazimayub786@gmail.com>
2026-06-14 20:38:33 -0500

    heading telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/subsystems/Drive.java
@@ -99,7 +99,7 @@ public class Drive extends SubsystemBase {
 
         telemetry.addData("X", this.getX());
         telemetry.addData("Y", this.getY());
-        telemetry.addData("Heading", this.getHeading());
+        telemetry.addData("Heading", Math.toDegrees(this.getHeading()));
         telemetry.update();
     }
 }
